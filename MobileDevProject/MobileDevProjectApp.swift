e1b5ae145506f3a17ef5ec35bdd07f4dd7f57521
Baptiste Keunebroek <baptiste.keunebroek@efrei.net>
2024-01-22 14:22:57 +0100

    Initial Commit

---
+++ MobileDevProject/MobileDevProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileDevProjectApp.swift
+//  MobileDevProject
+//
+//  Created by Baptiste Keunebroek on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MobileDevProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
